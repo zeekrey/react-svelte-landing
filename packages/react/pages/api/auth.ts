6a597517195cdfba7ae2d1b49164fdafc9864bcb
zeekrey <christian@krey.io>
2021-09-21 11:52:34 +0200

    refactor: removed example api folder.

--- packages/react/pages/api/auth.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { supabase } from "../../util/supabaseClient";
-
-const auth = (req: NextApiRequest, res: NextApiResponse) => {
-  supabase.auth.api.setAuthCookie(req, res);
-};
-
-export default auth;
